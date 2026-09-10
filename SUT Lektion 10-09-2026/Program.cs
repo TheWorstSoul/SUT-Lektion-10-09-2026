fd947b1938532dca2443f25dea7859330107f59b
TheWorstSoul <felix.grahs@gmail.com>
2026-09-10 10:55:02 +0100

    Add project files.

---
+++ SUT Lektion 10-09-2026/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SUT_Lektion_10_09_2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////immpilicet
+            //int valueint = 50;
+
+            //double valuedouble = valueint;
+
+
+            //Console.WriteLine(valuedouble);
+
+            ////expilicet
+
+
+            //float Fnumber = 5799999993.55f;
+
+            //int inNumber = (int)Fnumber; // type casting
+            //int InNumber2 = Convert.ToInt32(Fnumber); // Convert Class
+
+            //Console.WriteLine(inNumber);
+
+            //Console.WriteLine(InNumber2);
+
+
+// parse & Try parse
+           // int Result = 0;
+           // string stringNumber = "123";
+           // //int ConResult = int.parse(stringNumber );
+           // //int y = int.Parse(stringNumber);
+
+           //// Console.WriteLine(y);
+            
+           // bool IsConverted = int.TryParse(stringNumber, out Result);
+
+            
+           // if (IsConverted) 
+           // {
+
+           //     Console.WriteLine("result is : {0}", Result);
+            
+           // }
+
+           // else 
+           // {
+
+           //     Console.WriteLine("ples enter a vaild number");
+            
+           // }
+
+
+
+
+
+
+
+
+        }
+    }
+}
